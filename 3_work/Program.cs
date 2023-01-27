d013e799114c4fa1b0020a0587d7ef818f9d88c3
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-01-28 01:29:51 +0300

    3 work completed

---
+++ 3_work/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Укажите число: ");
+int number = int.Parse(Console.ReadLine());
+if(number%2 == 0){
+    Console.WriteLine("Да");
+}
+else{
+    Console.WriteLine("Нет");
+}
+
