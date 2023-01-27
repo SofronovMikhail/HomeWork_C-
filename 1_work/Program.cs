3ce19737d534c2f0586e300f3feb6ad4111dee0e
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-01-28 01:23:15 +0300

    1 work completed

---
+++ 1_work/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Укажите 1 число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Укажите 2 число: ");
+int numberB = int.Parse(Console.ReadLine());
+if(numberA > numberB){
+    Console.WriteLine($"max={numberA}");
+}
+else{
+    Console.WriteLine($"max={numberB}");
+}
