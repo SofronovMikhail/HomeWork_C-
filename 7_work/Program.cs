b8602e3856952bfe2459a11d0cbe2e11188fdfcc
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-01-31 23:18:25 +0300

    Д/З от 2 семинара

---
+++ 7_work/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите день недели: ");
+int num = int.Parse(Console.ReadLine());
+if(num <= 0 || num > 7){
+    Console.WriteLine("Не правильное число");
+    return;
+}
+if(num > 5 && num < 8){
+    Console.WriteLine("Да");
+}
+else{
+    Console.WriteLine("Нет");
+}
