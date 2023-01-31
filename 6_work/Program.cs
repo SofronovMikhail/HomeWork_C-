d80399830731df4698ed130b29f10bd81b569758
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-01-31 22:50:17 +0300

    Д/З от 2 семинара

---
+++ 6_work/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int num1 = number / 100;
+if(num1 > 0 && num1 < 10){
+    Console.WriteLine(number%10);
+}
+else{
+    Console.WriteLine("Не правильное число");
+}
