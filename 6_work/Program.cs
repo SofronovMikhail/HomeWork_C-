cc1a89a9ddea7acaf1b35a3ec4fb5a362326a8e4
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-01 21:40:01 +0300

    Последнее исправление 13 задачи

--- 6_work/Program.cs
+++ 6_work/Program.cs
@@ -1,21 +1,30 @@
 ﻿Console.Clear();
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
-int num1 = 10;
-int num2 = number / num1;
-if(num2 > 10 && num2 < 100){
-    Console.WriteLine(number%num1);
-    return;
+int num1 = 1;
+//int num2 = number / num1;
+//if(num2 > 10 && num2 < 100){
+ //   Console.WriteLine(number%num1);
+//  return;
+//}
+//else if(num2 < 10){
+   //Console.WriteLine("Третьей цифры нет.");
+   //return;
+//}
+//while(number / num1 > 99)
+//{
+ //    num1 = num1 * 10;
+//}
+if(number < 100){
+   Console.WriteLine("Третьей цифры нет."); 
 }
-else if(num2 < 10){
-   Console.WriteLine("Третьей цифры нет.");
-   return;
+else{
+  while(number / num1 > 99){
+    num1 = num1 * 10;
+  }  
+  int num2 = (number % num1) / (num1 / 10);
+  Console.WriteLine(num2);
 }
-while(number / num1 > 99)
-{
-     num1 = num1 * 10;
-}
-int num3 = (number % num1) / (num1 / 10);
-Console.WriteLine(num3);
+
 
 
