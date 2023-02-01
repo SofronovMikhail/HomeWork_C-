d5433e6d5c00622b46178f3a34739f07bd11eeef
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-01 20:21:41 +0300

    Исправленная 13 задача

--- 6_work/Program.cs
+++ 6_work/Program.cs
@@ -1,10 +1,21 @@
 ﻿Console.Clear();
-Console.Write("Введите трехзначное число: ");
+Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
-int num1 = number / 100;
-if(num1 > 0 && num1 < 10){
-    Console.WriteLine(number%10);
+int num1 = 10;
+int num2 = number / num1;
+if(num2 > 10 && num2 < 100){
+    Console.WriteLine(number%num1);
+    return;
 }
-else{
-    Console.WriteLine("Не правильное число");
+else if(num2 < 10){
+   Console.WriteLine("Третьей цифры нет.");
+   return;
 }
+while(number / num1 > 99)
+{
+     num1 = num1 * 10;
+}
+int num3 = (number % num1) / (num1 / 10);
+Console.WriteLine(num3);
+
+
