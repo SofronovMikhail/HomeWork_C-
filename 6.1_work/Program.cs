bbed6c34a0b696b1eb555141c314481ed717a04f
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-01 23:24:26 +0300

    решение 13 задачи через массив

---
+++ 6.1_work/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+string num = Convert.ToString(number[2]);
+Console.WriteLine($"{number} = {num}");
+if (number.Length < 3){
+    Console.WriteLine("третьего числа нет");
+}
