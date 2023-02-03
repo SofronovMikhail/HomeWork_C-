58e62ad821c29de4d1a5e7d0f2eca1b265a412ac
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-03 20:49:15 +0300

    3 Решение задачи 13

---
+++ 6.2_work/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if(number < 100){
+   Console.WriteLine("Третьей цифры нет."); 
+}
+else{
+  while(number > 1000){
+    number /=10;
+    }
+  Console.WriteLine(number%10);
+}
