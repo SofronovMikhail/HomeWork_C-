9d7dc0dce916d3d8bfd85198bb4e4578e9dc2284
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-04 03:13:59 +0300

    23 задача

---
+++ 10_work/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+int m = n;
+if(n < 0)
+{
+i = n;
+m=-n;
+n=-1;
+}
+int[] array = new int[m];
+for(int j = 0; i<=n; i++)
+{
+   array[j] = i * i * i;
+   j++;
+}
+for(int j = 0; j < m; j++)
+{
+    Console.WriteLine($"{array[j]}");
+}
