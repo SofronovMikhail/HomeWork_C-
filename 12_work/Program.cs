a63d58a2e3bdb53eebccf0a037c1e23b73759bac
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-07 16:47:18 +0300

    Задача №27

---
+++ 12_work/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int Sum(int num)
+{
+    int result = 0;
+    int count = 0;
+    int[] array = new int[num];
+    for(int i = 0; num >= 1; i++)
+    {
+        array[i] = num % 10;
+        num /= 10; 
+        count ++;  
+    }
+    for(int j = 0; j < count; j++)
+    {
+      result += array[j];  
+    }
+    return result;
+}
+Console.WriteLine($"сумма чисел = {Sum(n)}");
