d6ae97af163bb5db7ce259cb9500fce178ab183d
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-07 21:37:11 +0300

    Задача29

---
+++ 13_work/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите min значение массива: ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("Введите max значение массива: ");
+int max = int.Parse(Console.ReadLine());
+int[] Array(int min1, int max1)
+{
+    int length = 8;
+    int[] array1 = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        array1[i] = new Random().Next(min1, max1);
+    }
+    return array1;
+}
+int[] array2 = Array(min, max);
+Console.WriteLine($"[{String.Join(", ", array2)}]");
+
