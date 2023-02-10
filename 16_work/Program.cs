3da8623e7d63cb7165e7bc155bcb8f215b8d6ba7
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-11 02:08:52 +0300

    Задача № 34

---
+++ 16_work/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] Array(int size1)
+{
+    int[] arr = new int[size1];
+    for(int i = 0; i < size1; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+int[] array = Array(size);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int Count(int[] mas)
+{
+    int count = 0;
+    foreach (var item in mas)
+    {
+        if(item %2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine(Count(array));
