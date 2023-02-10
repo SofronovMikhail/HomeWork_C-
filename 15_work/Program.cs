7ad8368bb8d2fb518e7291514ae8d95d42929ff4
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-10 19:20:33 +0300

    Задача №37

---
+++ 15_work/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+Console.WriteLine("Введите значения массива через пробел: ");
+string arraySt = Console.ReadLine();
+int[] Array(string array)
+{
+    string[] array1 = array.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[array1.Length];
+    for(int i = 0; i < array1.Length; i++)
+    {
+        res[i] = int.Parse(array1[i]);
+    }
+    return res;
+}
+int[] arr = Array(arraySt); 
+Console.WriteLine(String.Join(" ", arr));
+
+int[] Array1(int[] array2)
+{
+    int[] arr = new int[array2.Length / 2];
+    if(array2.Length / 2 == 0){
+    for(int i = 0; i < array2.Length / 2; i++)
+    {
+       arr[i] = array2[i] * array2[array2.Length - i];
+    }
+    }
+    else{
+    for(int i = 0; i < (array2.Length / 2) + 1; i++)
+    {
+       arr[i] = array2[i] * array2[array2.Length - i];
+    }
+    }
+    return arr;
+}
+int[] arr1 = Array1(arr);
+Console.WriteLine(String.Join(" ", arr1));
+
