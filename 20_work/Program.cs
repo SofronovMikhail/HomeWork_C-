2d2481ab6f6b8318c4a33c5cf1d4ca0e64778465
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-15 15:13:18 +0300

    41 задача

---
+++ 20_work/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+Console.Write("Введите числа через пробел: ");
+string st = Console.ReadLine();
+int[]Arr(string st1)
+{
+string[] arr = st1.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] array = new int[arr.Length];
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(arr[i]);
+}
+return array;
+}
+int[] array = Arr(st);
+
+int Count(int[] arr1)
+{  
+    int count = 0;
+    foreach(int i in arr1)
+    {
+        if(i > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int count = Count(array);
+Console.WriteLine(count);
+
