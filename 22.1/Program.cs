f60678026abff83c8910d4b7a0fa64f54ffe447e
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-18 21:10:06 +0300

    50 c заполнением массива пользователем

---
+++ 22.1/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцoв: ");
+int columns = int.Parse(Console.ReadLine());
+
+Console.Write("Введите позицию в строке: ");
+int row = int.Parse(Console.ReadLine());
+
+Console.Write("Введите позицию в столбце: ");
+int col = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int m, int n)
+{
+int[,] result = new int[m,n];
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            Console.Write($"Введите элемент массива [{i},{j}]: ");
+            result[i,j] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine();
+    }
+    return result;
+}
+int[,] array = GetArray(rows, columns);
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Print(int[,] arr, int row1, int col1)
+{
+    if(row1 < arr.GetLength(0) || col1 < arr.GetLength(1))
+    {
+        Console.WriteLine($"Выбранное число: {arr[row1, col1]}");
+    }
+    else{
+        Console.WriteLine("Такого числа нет");
+    }
+}
+PrintArray(array);
+Print(array, row, col);
