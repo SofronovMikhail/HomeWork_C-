ff720061f6d379c0ade3e2b66cee0ca9ff6e3a68
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-24 18:07:35 +0300

    66

---
+++ 30/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 66:
+//Задайте значения  M и N.
+//Напишите программу, которая найдет сумму натуральных элементов в промежутке от M до N.
+
+
+Console.Clear();
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+int sum = 0;
+int SumNum(int n1, int m1)
+{   
+    if(m1 == n1)
+    {
+        return n1;
+    }
+    sum++;
+      return ((n1+sum) + SumNum(n1, m1 - 1));
+}
+
+Console.WriteLine(SumNum(N, M));
