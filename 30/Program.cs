d80d19d9464dda121d223bb3864b206e3c0c58b0
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-24 18:53:45 +0300

    66

--- 30/Program.cs
+++ 30/Program.cs
@@ -9,15 +9,13 @@ Console.Write("Введите число N: ");
 int N = int.Parse(Console.ReadLine());
 Console.Write("Введите число M: ");
 int M = int.Parse(Console.ReadLine());
-int sum = 0;
+
 int SumNum(int n1, int m1)
 {   
     if(m1 == n1)
     {
         return n1;
     }
-    sum++;
-      return ((n1+sum) + SumNum(n1, m1 - 1));
+      return (m1 + SumNum(n1, m1 - 1));
 }
-
 Console.WriteLine(SumNum(N, M));
